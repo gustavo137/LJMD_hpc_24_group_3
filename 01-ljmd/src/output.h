65dbf999cc1700e5ab825ceed7eefbdeb1881ebe
Ludwing Asturias <ludast@posteo.net>
2024-11-23 19:47:22 +0100

    Create project with separated files with the same cflags as the flat version

---
+++ 01-ljmd/src/output.h
@@ -0,0 +1,11 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include <stdio.h>
+#include <string.h>
+
+#include "mdsys.h"
+
+void output(mdsys_t *sys, FILE *erg, FILE *traj);
+
+#endif
---
+++ 01-ljmd/src/read_input.h
@@ -0,0 +1,10 @@
+#ifndef READ_INPUT_H
+#define READ_INPUT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int get_a_line(FILE *fp, char *buf);
+
+#endif
