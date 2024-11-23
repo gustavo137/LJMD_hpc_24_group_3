001b8abe5d1bb27b40ff24597f0ddf7f0cdde0a6
Ludwing Asturias <ludast@posteo.net>
2024-11-23 19:47:22 +0100

    Create project with separated files with the same cflags as the flat version

---
+++ 01-ljmd/src/force.h
@@ -0,0 +1,12 @@
+#ifndef FORCE_H
+#define FORCE_H
+#include "mdsys.h"
+#include "utilities.h"
+void force(mdsys_t *sys);
+#endif
+
+#ifndef VELVERLET_H
+#define VELVERLET_H
+#include "mdsys.h"
+void velverlet(mdsys_t *sys);
+#endif
---
+++ 01-ljmd/src/mdsys.h
@@ -0,0 +1,27 @@
+/* generic file- or pathname buffer length */
+#ifndef BLEN
+#define BLEN 200
+#endif
+
+/* a few physical constants */
+#ifndef PHYS_CONSTS
+#define PHYS_CONSTS
+extern const double kboltz;     /* boltzman constant in kcal/mol/K */
+extern const double mvsq2e;     /* m*v^2 in kcal/mol */
+#endif
+
+/* structure to hold the complete information
+ * about the MD system */
+#ifndef MDSYS_H
+#define MDSYS_H
+struct _mdsys {
+    int natoms,nfi,nsteps;
+    double dt, mass, epsilon, sigma, box, rcut;
+    double ekin, epot, temp;
+    double *rx, *ry, *rz;
+    double *vx, *vy, *vz;
+    double *fx, *fy, *fz;
+};
+typedef struct _mdsys mdsys_t;
+
+#endif
