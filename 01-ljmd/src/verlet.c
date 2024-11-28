7b54918ba916aacb6f2daf9b80805455e84e91b6
gustavo137 <gparedes@ictp.it>
2024-11-28 12:46:04 +0100

    Update ekin with OPENMP and take out the division in verlet

--- 01-ljmd/src/verlet.c
+++ 01-ljmd/src/verlet.c
@@ -7,11 +7,16 @@
 void velverlet1(mdsys_t *sys)
 {
     int i;
+    double mvsq2e_inv = 1.0/mvsq2e;
+    double mass_inv = 1.0/(sys->mass);
     /* first part: propagate velocities by half and positions by full step */
     for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        //sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+        //sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+        //sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        sys->vx[i] += 0.5*sys->dt * mvsq2e_inv * sys->fx[i] * mass_inv;
+        sys->vy[i] += 0.5*sys->dt * mvsq2e_inv * sys->fy[i] * mass_inv;
+        sys->vz[i] += 0.5*sys->dt * mvsq2e_inv * sys->fz[i] * mass_inv;
         sys->rx[i] += sys->dt*sys->vx[i];
         sys->ry[i] += sys->dt*sys->vy[i];
         sys->rz[i] += sys->dt*sys->vz[i];
@@ -21,11 +26,16 @@ void velverlet1(mdsys_t *sys)
 void velverlet2(mdsys_t *sys)
 {
     int i;
+    double mvsq2e_inv = 1.0/mvsq2e;
+    double mass_inv = 1.0/(sys->mass);
     /* second part: propagate velocities by another half step */
     for (i=0; i<sys->natoms; ++i) {
-        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
-        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
-        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        //sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+        //sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+        //sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        sys->vx[i] += 0.5*sys->dt * mvsq2e_inv * sys->fx[i] * mass_inv;
+        sys->vy[i] += 0.5*sys->dt * mvsq2e_inv * sys->fy[i] * mass_inv;
+        sys->vz[i] += 0.5*sys->dt * mvsq2e_inv * sys->fz[i] * mass_inv;
     }
 }
 
